82c54991ac8fa1b57328539a9d6aa5034fdaf628
aditij10 <aditi.jain28@gmail.com>
2023-02-25 04:46:01 +0530

    Add project files.

---
+++ Models/NasaData.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment.Models
+{
+    public class NasaData
+    {
+        public string? name { get; set; }
+        public int  id { get; set; }
+
+        //[DataType(DataType.Date)]
+        public string? nametype { get; set; }
+        public string? recclass { get; set; }
+        public string? mass { get; set; }
+        public string? fall { get; set; }
+        public string? year { get; set; }
+        public Decimal reclat { get; set; }
+        public Decimal reclong { get; set; }
+        public string? GeoLocation { get; set; }
+
+
+    }
+}
