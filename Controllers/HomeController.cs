4d2193a8f10479e08dfd504c6665f8156251a11b
aditij10 <aditi.jain28@gmail.com>
2023-02-25 13:34:05 +0530

    basic functionality to read data from CSV file is added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Assignment.Models;
+using CsvHelper;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace Assignment.Controllers
 {
@@ -17,6 +20,55 @@ namespace Assignment.Controllers
         {
             return View();
         }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(IFormFile upload)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (upload != null && upload.Length > 0)
+                {
+
+                    if (upload.FileName.EndsWith(".csv"))
+                    {
+                        DataTable dataTable = new DataTable();
+
+                        //Stream stream = file.InputStream;
+                        // Stream stream = new FileStream(file, FileMode.Create); 
+                        using(var stream = upload.OpenReadStream())
+                        using (CsvReader csvReader =
+                            new CsvReader(new StreamReader(stream), CultureInfo.InvariantCulture, true))
+                        {
+                            using(var dr = new CsvDataReader(csvReader))
+                            {
+                                dataTable.Load(dr);
+                            }
+                        }
+                            
+                        return View(dataTable);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please upload a csv file");
+                }
+            }
+            return View();
+        }
+
 
         public IActionResult Privacy()
         {
