f7d906ba1d755bf854c6cd49f7404085d4bfce01
aditij10 <aditi.jain28@gmail.com>
2023-02-27 06:50:39 +0530

    Pie Chart Updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,36 @@
 ﻿using Assignment.Models;
 using CsvHelper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Diagnostics;
 using System.Globalization;
+using System.Web;
 
 namespace Assignment.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        string path = "";
+      //  decimal dataPointFell = 0;
+       // decimal dataPointFound = 0;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return Redirect("/Home/Upload");
+
+            //return View();
+
         }
 
         public ActionResult Upload()
@@ -29,30 +41,60 @@ namespace Assignment.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Upload(IFormFile upload)
+        public ActionResult Upload(IFormFile uploadedFile)
         {
+            string filePath = string.Empty;
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            string contentRootPath = _webHostEnvironment.ContentRootPath;
+            decimal total_metiorites_landing = 0;
+            decimal total_metiorites_Fell = 0;
+            decimal total_metiorites_Found = 0;
+
+            path = "";
+            path = Path.Combine(webRootPath, "Uploads");
+            filePath = Path.Combine(path, uploadedFile.FileName);
+            using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                uploadedFile.CopyToAsync(fileStream);
+            }
+
             if (ModelState.IsValid)
             {
 
-                if (upload != null && upload.Length > 0)
+                if (uploadedFile != null && uploadedFile.Length > 0)
                 {
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
 
-                    if (upload.FileName.EndsWith(".csv"))
+                    if (uploadedFile.FileName.EndsWith(".csv"))
                     {
                         DataTable dataTable = new DataTable();
 
-                        //Stream stream = file.InputStream;
-                        // Stream stream = new FileStream(file, FileMode.Create); 
-                        using(var stream = upload.OpenReadStream())
+                        using (var stream = uploadedFile.OpenReadStream())
                         using (CsvReader csvReader =
                             new CsvReader(new StreamReader(stream), CultureInfo.InvariantCulture, true))
                         {
-                            using(var dr = new CsvDataReader(csvReader))
+                            using (var dr = new CsvDataReader(csvReader))
                             {
                                 dataTable.Load(dr);
                             }
                         }
-                            
+
+                        //PErform calculations to get data points for Pie chart
+                        //
+
+                        total_metiorites_landing = dataTable.AsEnumerable().Where(x => x["Fall"].ToString() == "Fell" || x["Fall"].ToString() == "Found").Count();
+                        total_metiorites_Fell = dataTable.AsEnumerable().Where(x => x["Fall"].ToString() == "Fell").Count();
+                        total_metiorites_Found = dataTable.AsEnumerable().Where(x => x["Fall"].ToString() == "Found").Count();
+
+                       decimal dataPointFell = Math.Round((total_metiorites_Fell / total_metiorites_landing) * 100, 2);
+                       decimal dataPointFound = Math.Round ((total_metiorites_Found / total_metiorites_landing) * 100,2);
+
+                        ViewData["dpFell"] = dataPointFell;
+                        ViewData["dpFound"] = dataPointFound;
+                        
                         return View(dataTable);
                     }
                     else
@@ -66,6 +108,15 @@ namespace Assignment.Controllers
                     ModelState.AddModelError("File", "Please upload a csv file");
                 }
             }
+            return View();
+        }
+
+        public IActionResult Charts(decimal dataPointFell , decimal dataPointFound )
+        {
+
+            ViewData["dpFell"] = dataPointFell;
+            ViewData["dpFound"] = dataPointFound;
+
             return View();
         }
 
